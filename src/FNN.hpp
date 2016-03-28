8e5d02d748c8b1824c04eae496a0b37e6ba3d6e7
Olivier Deiss <olivier.deiss@gmail.com>
2016-03-28 10:08:14 +0200

    identation

--- src/FNN.hpp
+++ src/FNN.hpp
@@ -283,10 +283,11 @@ typename FNN<T>::nabla_pair FNN<T>::backpropagation_cross_entropy(const Matrix<T
     /* feedforward */
     const Matrix<T>** activations = feedforward_complete(training_input);
     /* backpropagation */
-    const Matrix<T>** nabla_CW     = new const Matrix<T>*[nb_right_layers];
-    const Matrix<T>** nabla_CB     = new const Matrix<T>*[nb_right_layers];
-          Matrix<T>*  d           = new Matrix<T>(activations[nb_right_layers]);
+    const Matrix<T>** nabla_CW = new const Matrix<T>*[nb_right_layers];
+    const Matrix<T>** nabla_CB = new const Matrix<T>*[nb_right_layers];
+          Matrix<T>*  d        = new Matrix<T>(activations[nb_right_layers]);
     d->operator-(training_output);
+    d->print();
     Matrix<T>* at = new Matrix<T>(activations[nb_right_layers-1]); at->transpose();
     Matrix<T>* nw = new Matrix<T>(d);                              nw = nw->operator*(at);
     nabla_CW[nb_right_layers-1] = nw;
