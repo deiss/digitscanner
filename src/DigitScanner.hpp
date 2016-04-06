296c2307c6ad0553b5cfeb7b3102e27c57d20037
Olivier Deiss <olivier.deiss@gmail.com>
2016-04-06 18:27:39 +0200

    bug fixes

--- src/DigitScanner.hpp
+++ src/DigitScanner.hpp
@@ -306,16 +306,16 @@ void DigitScanner<T>::train(std::string path_data, const int nb_images, const in
         for(int j=0 ; j<nb_threads ; j++) {
             /* first thread shows progress */
             if(j==0) {
-                threads.push_back(&DigitScanner<T>::train_thread, this, path_data, nb_images, nb_images_to_skip, nb_epoch, batch_len, eta, alpha, nb_threads, 0, nb_batches_per_subsets*batch_len, i, shuffle);
+                threads.push_back(std::thread(&DigitScanner<T>::train_thread, this, path_data, nb_images, nb_images_to_skip, nb_epoch, batch_len, eta, alpha, nb_threads, 0, nb_batches_per_subsets*batch_len, i, shuffle, true));
             }
             /* last thread computes maximum batches available */
             else if(j==nb_threads-1) {
                 int nb_batches_available = nb_batches - j*nb_batches_per_subsets;
-                threads.push_back(&DigitScanner<T>::train_thread, this, path_data, nb_images, nb_images_to_skip, nb_epoch, batch_len, eta, alpha, nb_threads, j*nb_batches_per_subsets*batch_len, (j*nb_batches_per_subsets + nb_batches_available)*batch_len, i, shuffle);
+                threads.push_back(std::thread(&DigitScanner<T>::train_thread, this, path_data, nb_images, nb_images_to_skip, nb_epoch, batch_len, eta, alpha, nb_threads, j*nb_batches_per_subsets*batch_len, (j*nb_batches_per_subsets + nb_batches_available)*batch_len, i, shuffle, false));
             }
             /* middle threads compute nb_batches_per_subset batches */
             else {
-                threads.push_back(&DigitScanner<T>::train_thread, this, path_data, nb_images, nb_images_to_skip, nb_epoch, batch_len, eta, alpha, nb_threads, j*nb_batches_per_subsets*batch_len, (j+1)*nb_batches_per_subsets*batch_len, i, shuffle);
+                threads.push_back(std::thread(&DigitScanner<T>::train_thread, this, path_data, nb_images, nb_images_to_skip, nb_epoch, batch_len, eta, alpha, nb_threads, j*nb_batches_per_subsets*batch_len, (j+1)*nb_batches_per_subsets*batch_len, i, shuffle, false));
             }
         }
         /* join all threads */
